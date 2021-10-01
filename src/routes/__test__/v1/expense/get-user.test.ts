a168123e2a4c17c20eb182c5e43e60ca86a5a7dc
rupaak <rupakt525@gmail.com>
2021-10-01 22:18:54 +0545

    get use expense api

---
+++ src/routes/__test__/v1/expense/get-user.test.ts
@@ -0,0 +1,49 @@
+import request from "supertest";
+import { app } from "../../../../app";
+import "../../../../test/setup";
+
+it("should not throw 404 while calling get endpoint", async () => {
+    const res = await request(app).get("/api/v1/expenses/userexpense").send({});
+    expect(res.status).not.toEqual(404);
+});
+
+it("should not throw 401 not authorized if token invalid", async () => {    
+    const res= await request(app).get("/api/v1/expenses/userexpense")
+        .set({
+            Authorization: `Bearer token`
+        })
+    .expect(401)
+});
+
+it("should throw data while calling get endpoint", async () => {
+    const token= await global.signin()
+    
+    const res= await request(app).get("/api/v1/expenses/userexpense")
+        .set({
+            Authorization: `Bearer ${token}`
+        })
+    .expect(200)
+    expect(res.body.data).toBeDefined()
+});
+
+it("should throw data only of the user", async () => {
+    const token= await global.signin()
+
+    const res= await request(app).post("/api/v1/expenses")
+    .set({
+        Authorization: `Bearer ${token}`
+    })
+    .send({
+        name: "This is length",
+        amount: 5000,
+        date: '2057/11/11'
+    })
+    .expect(201)
+    
+    const res2= await request(app).get("/api/v1/expenses/userexpense")
+        .set({
+            Authorization: `Bearer ${token}`
+        })
+    .expect(200)
+    expect(res2.body.data[0].user).toEqual(res.body.data.user)
+});
